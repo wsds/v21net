0771935319fe2f64d91328c7676faf4df3e40b18
wsds@server <wsds88801@163.com>
2013-02-16 18:03:07 +0800

    servercluster is add for taking it easy to start servers. and the offline tools has been taken superiority.

---
+++ nodeserver/servercluster.js
@@ -0,0 +1,11 @@
+
+
+
+var weibopost = require('./weibopost/weibopost.js');
+console.log("weibopost is starting");
+
+var weiboauth = require('./weiboauth/connect_with_oauth_middleware.js');
+console.log("weiboauth is starting");
+
+var imageupload = require('./imageupload/index.js');
+console.log("imageupload is starting");
