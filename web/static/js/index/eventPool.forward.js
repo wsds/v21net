dd76a87d2894dccae32417cfd80e61599c89a2c2
wsds@server <wsds88801@163.com>
2013-05-02 18:55:41 +0800

    The UE of forwarding has been accomplished.

--- web/static/js/index/eventPool.forward.js
+++ web/static/js/index/eventPool.forward.js
@@ -1,6 +1,23 @@
 eventPool.main_forward = function (status, area) {
 
 
+    $(".forward_post_show", area).click(function () {
+        var postID = $(this).attr("postid");
+        var forward_post_panel = $(".forward_post_panel[postID='" + postID + "']");
+        forward_post_panel.toggleClass("hide");
+    });
+
+    $(".cancel_forward_post", area).click(function () {
+        var postID = $(this).attr("postid");
+        var forward_post_panel = $(".forward_post_panel[postID='" + postID + "']");
+        forward_post_panel.addClass("hide");
+    });
+
+    $(".forward_post", area).click(function () {
+        var postID = $(this).attr("postid");
+        var forward_post_panel = $(".forward_post_panel[postID='" + postID + "']");
+        forward_post_panel.addClass("hide");
+    });
 
 };
 
