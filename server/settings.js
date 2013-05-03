d0d221d7d68a4ba3749892f76561e25fc344a4ab
wsds@server <wsds88801@163.com>
2013-05-03 18:24:14 +0800

    deploy to server.

--- server/settings.js
+++ server/settings.js
@@ -3,9 +3,11 @@ root.globaldata = {};
 var serverSetting = {};
 globaldata.serverSetting = serverSetting;
 
-serverSetting.appkey = "2445517113";
-serverSetting.callbackUrl = "http://www.weibo.com/oauth/callback";//http://offline.wlm1001.com/oauth/callback
+//serverSetting.appkey = "2445517113";
+//serverSetting.callbackUrl = "http://www.wlwm.com/oauth/callback";//http://offline.wlm1001.com/oauth/callback
+serverSetting.appkey = "3322737363";
+serverSetting.callbackUrl = "http://offline.wlm1001.com/oauth/callback";//http://offline.wlm1001.com/oauth/callback
 
-serverSetting.imageFolder = "E://nginx//upload//";
+serverSetting.imageFolder = "//upload//weibo//";
 
 module.exports = serverSetting;
