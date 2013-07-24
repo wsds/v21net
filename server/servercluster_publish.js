c258e5c1bf4319a2fcf01848ef40c3f15ec7748e
wsds <wsds888@163.com>
2013-07-24 19:22:00 +0800

    turn on the publish server.

--- server/servercluster_publish.js
+++ server/servercluster_publish.js
@@ -3,5 +3,5 @@ var serverSetting = require('./settings.js');
 var weibopost = require('./apiserver/main.js');//8061
 //console.log("apiserver is starting");
 
-//var publishing = require('./publishing/main.js');//8063
-////console.log("publishing is starting");
+var publishing = require('./publishing/main.js');//8063
+//console.log("publishing is starting");
